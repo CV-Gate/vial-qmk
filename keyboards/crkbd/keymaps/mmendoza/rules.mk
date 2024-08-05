919d290d84739a786def858918723ae122a82dde
Miguel mendoza <miguel.mendoza@vizzuality.com>
2024-08-05 17:51:03 +0200

    implements left and right firmwares for crkbd

--- keyboards/crkbd/keymaps/mmendoza/rules.mk
+++ keyboards/crkbd/keymaps/mmendoza/rules.mk
@@ -5,9 +5,15 @@ LTO_ENABLE          = yes
 RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
-OLED_ENABLE         = no
-OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = yes
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
+EXTRAKEY_ENABLE     = no
 COMBO_ENABLE        = no
-
 QMK_SETTINGS        = no
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AVR_USE_MINIMAL_PRINTF = yes
